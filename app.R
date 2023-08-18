94e323d9309d7b620493ed441994e2685c8874cf
Kiran Subramanian S <kiransubramanians@Kirans-MacBook-Pro.local>
2023-08-18 18:21:43 +0100

    Changes in Code comment

--- app.R
+++ app.R
@@ -9,16 +9,16 @@ model <- load_model_tf("model_augmented/")
 
 # Define the UI
 ui <- fluidPage(
-  # App title ----
+  # App title 
   titlePanel("Lung Cancer Detection"),
-  # Sidebar layout with input and output definitions ----
+  # Sidebar layout with input and output definitions 
   sidebarLayout(
-    # Sidebar panel for inputs ----
+    # Sidebar panel for inputs
     sidebarPanel(
       # Input: File upload
       fileInput("image_path", label = "Input a PNG image")
     ),
-    # Main panel for displaying outputs ----
+    # Main panel for displaying outputs
     mainPanel(
       #text out
       textOutput(outputId = "prediction"),
@@ -28,7 +28,7 @@ ui <- fluidPage(
 )
 
 
-# Define server logic required to draw a histogram ----
+# Define server logic
 server <- function(input, output) {
   #loading the image as png
   image1 <- reactive({
@@ -42,15 +42,15 @@ server <- function(input, output) {
   })
   
   output$prediction <- renderText({
-    #rescaling in reshaping to modelreqirement
+    #Re-scaling in reshaping to modelrequirement
     img_test_app <- image_load(image(), target_size = c(255, 255), 
                                grayscale = F # Set FALSE if image is RGB
     )
     #convert to array
     x <- image_to_array(img_test_app)
-    #resaping to predict
+    #reshaping to predict
     x <- array_reshape(x, c(1, dim(x)))
-    x <- x/255 # rescale image pixel
+    x <- x/255 # Re-scale image pixel
     class_labels <- c("Adenocarcinoma", "Large cell carcinoma", "Normal CT Scan", "Squamous cell carcinoma")
     #finding the  probability each class
     values=predict(model,x)
@@ -59,7 +59,7 @@ server <- function(input, output) {
     # Randomly select one string from the list
     paste0("The predicted Lung Cancer Type is ", class_labels[index] ," - ",max_value,"%")
   })
-  #rendering the uploaded inage
+  #rendering the uploaded image
   output$image <- renderPlot({
     plot(as.raster(image1()))
   })
